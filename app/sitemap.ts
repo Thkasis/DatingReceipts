d6b2c24c4cec67a3f17c5523a8b00ca589ac469d
Sepehr <devsepehr@gmail.com>
2025-12-28 11:09:18 -0500

    Fix: Add force-static export for robots.txt and sitemap.xml for Cloudflare static export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,6 +5,8 @@
 
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://datingreceipts.com'
 
